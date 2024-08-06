750b033d6ca74ecb9aada159a275875ccabbb355
Ho Hoan Nghia <hoannghia_ho@sutd.edu.sg>
2024-08-06 11:25:28 +0800

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/nghiaho/ros1_ws/src/jabez_detect_marker
+# Build directory: /home/nghiaho/ros1_ws/src/jabez_detect_marker/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
