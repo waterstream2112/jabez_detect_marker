273d8e0606b946babe93336a79da543fd064d668
Ho Hoan Nghia <hoannghia_ho@sutd.edu.sg>
2024-08-07 16:11:02 +0800

    temp save

--- scripts/run_relay.sh
+++ scripts/run_relay.sh
@@ -4,6 +4,6 @@ cd ~/ghost_ws
 source /opt/ros/noetic/setup.bash
 source devel/setup.bash
 
-rosrun topic_tools drop /argus/ar0234_front_right/image_raw 1 2 /camera &
-rosrun topic_tools relay /trigger_detection /trigger_detection_ghost &
-rosrun topic_tools relay /detection_result_orin /detection_result & 
+rosrun topic_tools drop /argus/ar0234_front_right/image_raw 1 2 /camera #&
+# rosrun topic_tools relay /trigger_detection /trigger_detection_ghost &
+# rosrun topic_tools relay /detection_result_orin /detection_result & 
